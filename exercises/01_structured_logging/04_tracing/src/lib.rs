4e742ff0e77dbb452e0a441492a7c5976fd70227
mind1949 <lianjie1949@gmail.com>
2025-03-15 18:23:28 +0800

    complete 01.04

--- exercises/01_structured_logging/04_tracing/src/lib.rs
+++ exercises/01_structured_logging/04_tracing/src/lib.rs
@@ -6,6 +6,7 @@
 mod subscriber;
 
 pub use subscriber::init_test_subscriber;
+pub use tracing;
 
 /// Given a list of order numbers, compute the total price.
 ///
@@ -15,10 +16,16 @@ pub use subscriber::init_test_subscriber;
 /// We don't care about capturing the outcome of each unit of work (for now).
 ///
 /// Refer to the test files for the expected output format.
+#[tracing::instrument("process total price", skip_all)]
 pub fn get_total(order_numbers: &[u64]) -> Result<u64, anyhow::Error> {
     // Tip: use `tracing::info_span!` to create a new span.
     // You'll have to learn about the *RAII guard* pattern!
-    todo!()
+    let mut total = 0;
+    for order_number in order_numbers {
+        let order = get_order_details(*order_number)?;
+        total += order.price;
+    }
+    Ok(total)
 }
 
 pub struct OrderDetails {
@@ -27,6 +34,7 @@ pub struct OrderDetails {
 }
 
 /// A dummy function to simulate what would normally be a database query.
+#[tracing::instrument("retrieve order", skip_all)]
 fn get_order_details(order_number: u64) -> Result<OrderDetails, anyhow::Error> {
     if order_number % 4 == 0 {
         Err(anyhow::anyhow!("Failed to talk to the database"))

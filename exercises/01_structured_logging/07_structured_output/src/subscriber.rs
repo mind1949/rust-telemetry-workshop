bfbc3b37ea04fae79e35d2447d7a0dd3582c5c31
mind1949 <lianjie1949@gmail.com>
2025-03-15 19:24:02 +0800

    complete 01.07

--- exercises/01_structured_logging/07_structured_output/src/subscriber.rs
+++ exercises/01_structured_logging/07_structured_output/src/subscriber.rs
@@ -9,6 +9,10 @@ pub fn init_test_subscriber() -> MockWriter {
         .with_span_events(FmtSpan::NEW | FmtSpan::EXIT)
         // TODO: we want JSON! Check out the test suite to understand the expected output.
         // Make sure to **flatten your events**!
+        .without_time()
+        .with_level(false)
+        .json()
+        .flatten_event(true)
         .init();
     writer2
 }
